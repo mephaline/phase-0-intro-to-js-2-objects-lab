a80949f76605e44b2377f97ab226838e58dc98d1
mephaline <suaramos88@gmail.com>
2023-09-12 16:43:17 -0500

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+   
+    const updatedEmployee = { ...employee };
+
+    updatedEmployee[key] = value;
+  
+    return updatedEmployee;
+  }
+  const employee = {
+    name: "John",
+    age: 30,
+    department: "HR",
+  };
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+
+    return employee;
+  }
+function deleteFromEmployeeByKey(employee, key) {
+
+    const clonedEmployee = { ...employee };
+    delete clonedEmployee[key];
+    return clonedEmployee;
+    }
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    if (employee.hasOwnProperty(key))
+        delete employee[key];
+        return employee;
+        }
